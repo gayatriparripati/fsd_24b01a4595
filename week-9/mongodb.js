94e1d0d97a429a41ccfb76cae12441f341045b6f
gayatriparripati <gayatriparripati11@gmail.com>
2026-09-25 14:21:58 +0530

    Add week 9

---
+++ week-9/mongodb.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log("MongoDB connection error:", err));
+
+app.get("/", (req, res) => {
+    res.send("Week 9 MongoDB project is running");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
